ecf13c404581be3d27498da1854d1a36c505671e
John Vasileff <john@vasileff.com>
2025-04-29 23:03:19 -0400

    Update to latest atpacks

--- versions.sh
+++ versions.sh
@@ -6,7 +6,7 @@ export LIBC_VERSION=2.2.1
 
 export PACKS=(
     Atmel.ATtiny_DFP.2.0.368.atpack
-    Atmel.AVR-Ex_DFP.2.10.205.atpack
+    Atmel.AVR-Ex_DFP.2.11.221.atpack
     Atmel.ATmega_DFP.2.2.509.atpack
-    Atmel.AVR-Dx_DFP.2.6.303.atpack
+    Atmel.AVR-Dx_DFP.2.7.321.atpack
 )
